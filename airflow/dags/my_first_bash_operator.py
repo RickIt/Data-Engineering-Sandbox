8ea0017e672afcda351c7a3cb6c9feda1d590085
Ricardo Nascimento de Souza <ricardonasdesouza@gmail.com>
2022-09-08 00:10:54 -0300

    aprendendo a criar dags

---
+++ airflow/dags/my_first_bash_operator.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+#airflow 2.0
+#from airflow.operators.bash import BashOperator
+
+#airflow 1.10
+from airflow.operators.bash_operator import BashOperator
+
+
+default_args = {
+    'owner': 'ricardo',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='minha_primeira_dag_v4',
+    default_args=default_args,
+    description='Esta é minha primeira dag',
+    start_date=datetime(2022, 9, 7),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='primeira_task',
+        bash_command="echo Olá Mundo, esta é a minha primeira task"
+    )
+
+    task2 = BashOperator(
+        task_id='segunda_task',
+        bash_command="echo hey, eu sou a segunda task e serei executada após a task1"
+    )
+
+    task3 = BashOperator(
+        task_id='terceira_task',
+        bash_command="""Esta é a terceira task e irá executar depois da task1 
+                        e ao mesmo tempo que a task2"""
+    )
+
+    #task1.set_downstream(task2)
+    #task1.set_downstream(task3)
+    task1 >> task2
+    task1 >> task3
