2e234e706e2e9a4dbf13a54ec841ad9278cc3721
Ricardo Nascimento de Souza <ricardonasdesouza@gmail.com>
2022-09-11 00:56:55 -0300

    Minha primeira dag com xcom

--- airflow/dags/create_dag_with_python_operator.py
+++ airflow/dags/create_dag_with_python_operator.py
@@ -12,20 +12,32 @@ default_args = {
 }
 
 
-def greet():
-    print('Hello World!')
+def greet(age,**context):
+    name = context['ti'].xcom_pull(task_ids='get_name')
+    print(f"Hello World! My name is {name}, "
+          f"and I am {age} years old!")
+
+def get_name():
+    return 'Ricardo'
 
 
 with DAG(
     default_args=default_args,
-    dag_id='Minha_primeira_dag_com_python_operator_v01',
-    description='Esta é minha primeira dag com python oprator'
-    start_date=datetime(2022, 9, 9)
+    dag_id='Minha_primeira_dag_com_python_operator_v04',
+    description='Esta é minha primeira dag com python operator',
+    start_date=datetime(2022, 9, 9),
     schedule_interval='@daily'
 ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet
+        provide_context=True,
+        python_callable=greet,
+        op_kwargs={'age': 20}
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name        
+    )
+
+    task2 >> task1
